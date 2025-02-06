b48079e9c1873727b08ac8c3c6ccd52d0c435d2f
Kerama420 <kerama369@gmail.com>
2025-02-06 23:58:43 +0300

    made changes to navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <h1 className="title">Films-r-Us</h1>
+      <Link to="/" className="logo">
+        Films-r-us
+      </Link>
       <div className="nav-links">
         <Link to="/">Home</Link>
-        <Link to="/favourites">Favourites</Link>
+        <Link to="/favorites">Favorites</Link>
         <Link to="/watchlist">Watchlist</Link>
         <Link to="/watched">Watched</Link>
       </div>
